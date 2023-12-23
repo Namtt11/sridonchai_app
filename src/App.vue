fcd3ffe71aa731b975cc7ca2094c455cd4fac0de
Namtt11 <89740393+Namtt11@users.noreply.github.com>
2023-12-23 22:26:08 +0700

    vue

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1 class="text-2xl">Home</h1>
+</template>
+
